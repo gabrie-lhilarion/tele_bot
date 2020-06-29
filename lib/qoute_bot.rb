9830a9a21a8983b4d8381c9e09a954b72185ea9f
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-06-29 11:27:47 -0600

    updateed qout_bot.rb and motivation.rb

---
+++ lib/qoute_bot.rb
@@ -0,0 +1,28 @@
+require 'telegram/bot'
+require_relative 'motivation.rb'
+
+
+class MyBot
+  def initialize
+    token = '1247899419:AAEDYdLqMaIbKITIFh5YtIY-xk05X0VSjTo'
+
+  Telegram::Bot::Client.run(token) do |bot|
+    bot.listen do |message|
+      case message.text
+      when '/start'
+
+        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , welcome! Use  /start to start the bot,  /stop to end the bot, /motivate to get a diffrent motivational quote everytime you request for it or /joke to get a joke everytime you request for it")
+
+      when '/stop'
+
+        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
+      when '/motivate'
+        values = Motivate.new
+        value = values.select_random
+        bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
+      else bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
+      end
+    end
+  end
+  end
+end
